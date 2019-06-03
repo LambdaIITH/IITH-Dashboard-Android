d3e69fd247299f486eb16e2b0c44883fc8343bdc
Aayush Goyal <aayush.goyal54@gmail.com>
2019-06-03 22:45:36 +0530

    Ported to material 2.0

--- app/src/main/java/com/lambda/iith/dashboard/CabSharing.java
+++ app/src/main/java/com/lambda/iith/dashboard/CabSharing.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +15,16 @@ import android.support.design.widget.FloatingActionButton;
 
 public class CabSharing extends Fragment {
     private FloatingActionButton register;
+    public int flag;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        flag = 0;
         final View view = inflater.inflate(R.layout.cab_sharing, container , false);
 
         register = (FloatingActionButton) view.findViewById(R.id.cab_sharing_register);
+
 
         register.setOnClickListener(new View.OnClickListener() {
 
@@ -29,7 +33,9 @@ public class CabSharing extends Fragment {
                 register.hide();
                 FragmentManager fm = getFragmentManager();
                 CabSharingMenu cabSharingMenu = CabSharingMenu.newInstance("Menu");
+
                 cabSharingMenu.show(fm , "BottomMenu");
+
 
             }
         });
@@ -38,7 +44,15 @@ public class CabSharing extends Fragment {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        //getFragmentManager().beginTransaction().replace(R.id.fragmentlayout , new CabSharing()).commit();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
-
+    }
 }
--- app/src/main/java/com/lambda/iith/dashboard/CabSharingMenu.java
+++ app/src/main/java/com/lambda/iith/dashboard/CabSharingMenu.java
@@ -64,4 +64,8 @@ public class CabSharingMenu extends DialogFragment {
 
         return frag;
     }
+
+
 }
+
+
