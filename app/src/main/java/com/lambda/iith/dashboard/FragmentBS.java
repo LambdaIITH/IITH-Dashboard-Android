4f9f1ecf92d6472012a81093a60aa1ed920561a5
Kkuntal990 <kokatekuntal@gmail.com>
2019-06-21 20:26:52 +0530

    Bus Schedule bugs

--- app/src/main/java/com/lambda/iith/dashboard/FragmentBS.java
+++ app/src/main/java/com/lambda/iith/dashboard/FragmentBS.java
@@ -13,32 +13,18 @@ import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
-import Adapters.CustomAdapter;
-import structs.BsItem;
 
 
 public class FragmentBS extends Fragment implements AdapterView.OnItemSelectedListener {
-    Spinner spinner1;
-    private ArrayList<BsItem> mBsItems;
-    private CustomAdapter mAdapter;
+    Spinner spinner2;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.bs_layout,container,false);
-        initList();
-        spinner1 = (Spinner) view.findViewById(R.id.spinner_1);
-
-
-        mAdapter = new CustomAdapter(getActivity(),mBsItems);
-        spinner1.setAdapter(mAdapter);
-        spinner1.setSelection(mAdapter.getCount());
-        spinner1.setOnItemSelectedListener(this);
-
-
+        spinner2 = (Spinner) view.findViewById(R.id.spinner_2);
+        spinner2.setOnItemSelectedListener(this);
 
 
         return view;
@@ -47,26 +33,19 @@ public class FragmentBS extends Fragment implements AdapterView.OnItemSelectedLi
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        BsItem clickedItem = (BsItem) parent.getItemAtPosition(position);
-        String clickedString = clickedItem.getBsType();
-        Toast.makeText(getActivity(),clickedString + " selected",Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(getActivity(),parent.getItemAtPosition(position).toString() + " selected",Toast.LENGTH_SHORT).show();
 
     }
+
+
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
-    private void initList() {
-        mBsItems = new ArrayList<>();
-        mBsItems.add(new BsItem("Minibuses",R.mipmap.drop));
-        mBsItems.add(new BsItem("Lingampally-IITH",R.mipmap.drop));
-        mBsItems.add(new BsItem("Maingate-Hostel",R.mipmap.drop));
-        mBsItems.add(new BsItem("ODF-Kandi",R.mipmap.drop));
-        mBsItems.add(new BsItem("Select Type",R.mipmap.drop));
 
-    }
 
 
 }
