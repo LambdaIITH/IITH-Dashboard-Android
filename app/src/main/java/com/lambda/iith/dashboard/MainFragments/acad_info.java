d20ae8e07e1f57d34999b4278cef5c58e3b421a4
aayush <aayush.goyal54@gmail.com>
2021-05-20 23:55:51 +0530

    Removed Curriculum,Handbook

--- app/src/main/java/com/lambda/iith/dashboard/MainFragments/acad_info.java
+++ app/src/main/java/com/lambda/iith/dashboard/MainFragments/acad_info.java
@@ -41,21 +41,21 @@ public class acad_info extends Fragment {
 //            }
 //        });
 
-        final Button curriculum = (Button) view.findViewById(R.id.curriculum);
-        ViewGroup.LayoutParams params = curriculum.getLayoutParams();
-        params.width = -5+Launch.width/3;
-        curriculum.setLayoutParams(params);
-
-
-        final Button handbooks = (Button) view.findViewById(R.id.handbooks);
-        ViewGroup.LayoutParams params1 = handbooks.getLayoutParams();
-        params1.width = -5+ Launch.width/3;
-        handbooks.setLayoutParams(params1);
-
-        final Button acad_cal = (Button) view.findViewById(R.id.acad_calender);
-        ViewGroup.LayoutParams params2 = acad_cal.getLayoutParams();
-        params2.width = -5+Launch.width/3;
-        acad_cal.setLayoutParams(params2);
+//        final Button curriculum = (Button) view.findViewById(R.id.curriculum);
+//        ViewGroup.LayoutParams params = curriculum.getLayoutParams();
+//        params.width = -5+Launch.width/3;
+//        curriculum.setLayoutParams(params);
+//
+//
+//        final Button handbooks = (Button) view.findViewById(R.id.handbooks);
+//        ViewGroup.LayoutParams params1 = handbooks.getLayoutParams();
+//        params1.width = -5+ Launch.width/3;
+//        handbooks.setLayoutParams(params1);
+//
+//        final Button acad_cal = (Button) view.findViewById(R.id.acad_calender);
+//        ViewGroup.LayoutParams params2 = acad_cal.getLayoutParams();
+//        params2.width = -5+Launch.width/3;
+//        acad_cal.setLayoutParams(params2);
 
         webView = view.findViewById(R.id.webview);
         //webView.setLayoutParams(new LinearLayout.LayoutParams(Launch.width , Launch.height*));
@@ -69,29 +69,29 @@ public class acad_info extends Fragment {
                 Base64.NO_PADDING);
         webView.loadData(encodedHtml, "text/html", "base64");
 
-        curriculum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent curri=new Intent(getActivity(), curriculum.class);
-                startActivity(curri);
-            }
-        });
+//        curriculum.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent curri=new Intent(getActivity(), curriculum.class);
+//                startActivity(curri);
+//            }
+//        });
 
-        acad_cal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://calendar.google.com/calendar/u/0/r?cid=c_ll73em8s81nhu8nbjvggqhmbgc@group.calendar.google.com")));
+//        acad_cal.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://calendar.google.com/calendar/u/0/r?cid=c_ll73em8s81nhu8nbjvggqhmbgc@group.calendar.google.com")));
+//
+//            }
+//        });
 
-            }
-        });
-
-        handbooks.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://iith.dev/curriculum/academic_handbook.pdf")));
-
-            }
-        });
+//        handbooks.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://iith.dev/curriculum/academic_handbook.pdf")));
+//
+//            }
+//        });
 
 
 
